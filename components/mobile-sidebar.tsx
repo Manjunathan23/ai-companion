528955426161fe865a24c39dd2903bcc658f95f8
Manjunathan23 <104411355+Manjunathan23@users.noreply.github.com>
2024-02-25 15:40:44 +0530

    sidebar-fix

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,14 +1,17 @@
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { Sidebar } from "./sidebar";
-export const MobileSidebar = () => {
+import { checkSubscription } from "@/lib/subscription";
+export const MobileSidebar = async() => {
+  const isPro = await checkSubscription();
+
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar />
+      <Sidebar isPro={isPro}/>
       </SheetContent>
     </Sheet>
   );
